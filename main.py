cb552a7a87954d66b4e4b65ba1a1fd0c22f3ecff
AndrewGibson12 <gibsonan@students.dsbn.org>
2025-03-24 12:28:25 -0400

    Add files via upload

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
+# Name: A. Gibson
+# Title: Average
+# Date: march 24, 2025
+# Description: This program takes 100 random numbers then finds the average
+import random
+num={random.randint(0,100) for i in range(100)}
 
+print("Your 100 numbers are,")
+print(num)
+
+Average = sum(num)/len(num)
+print("The average of the 100 numbers is", Average)
